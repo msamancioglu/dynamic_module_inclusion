97998da5ae058ddfa4dd425ae98585b2d4b4ac5e
Mustafa Samancioglu <39189037+msamancioglu@users.noreply.github.com>
2021-12-05 21:25:19 +0300

    added validation for no file found

--- main.py
+++ main.py
@@ -16,4 +16,5 @@ if __name__ == "__main__":
         print(ret)
         end = datetime.now()
         print(end - start)
-    
+    else:
+        print(f"no magic file with name [{sys.argv[1]}] found under //dynamic_files folder")  
